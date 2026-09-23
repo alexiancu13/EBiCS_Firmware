cd79cb1d957c2af96f88f34833bbe1b5c2f2ac18
alexiancu13 <alexianxu4@gmail.com>
2026-09-23 13:29:34 +0300

    M365config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -18,10 +18,10 @@
 #define REGEN_MAX_CURRENT 10000
 
 // maximum current for field weakening in mA
-#define FIELD_WEAKNING_CURRENT_MAX 0 //max id
+#define FIELD_WEAKNING_CURRENT_MAX 900 //max id
 
 // ADC channels
-#define ADC_VOLTAGE 0
+#define ADC_VOLTAGE 52
 #define ADC_THROTTLE 1
 #define ADC_TEMP 2
 
